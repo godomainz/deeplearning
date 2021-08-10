33ea2f80db8a79be24f60704608fe2af92d52db7
Akila Mendis <akila@oneit.com.au>
2021-08-10 13:55:07 +0530

    Rnn saving a model

--- vol1_supervised_deep_learning/Part 3 - Recurrent Neural Networks (RNN)/Section 12 - Building a RNN/rnn.py
+++ vol1_supervised_deep_learning/Part 3 - Recurrent Neural Networks (RNN)/Section 12 - Building a RNN/rnn.py
@@ -1,16 +1,25 @@
+import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-
+from sklearn.preprocessing import MinMaxScaler
+import os
 
 class Rnn:
+    # Prepare a directory to store all the checkpoints.
+    checkpoint_dir = 'ckpt'
 
     # Importing the Training Set
     dataset_train = pd.read_csv('Google_Stock_Price_Train.csv')
     training_set = dataset_train.iloc[:, 1:2].values
 
+    # Feature scaling
+    sc = MinMaxScaler(feature_range=(0, 1), copy=True)
+
     def __init__(self):
-        print("RNN")
+        print("tf.__version__ " + tf.__version__)
+        if not os.path.exists(self.checkpoint_dir):
+            os.makedirs(self.checkpoint_dir)
 
 
 if __name__ == "__main__":
