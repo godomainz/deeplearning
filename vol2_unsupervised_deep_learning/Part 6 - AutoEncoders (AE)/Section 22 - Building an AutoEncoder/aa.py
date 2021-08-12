e85003ace39b57aa43d0d0843adefe140161ca56
Akila Mendis <akila@oneit.com.au>
2021-08-12 15:13:04 +0530

    Building an AutoEncoder - Step 2

--- vol2_unsupervised_deep_learning/Part 6 - AutoEncoders (AE)/Section 22 - Building an AutoEncoder/aa.py
+++ vol2_unsupervised_deep_learning/Part 6 - AutoEncoders (AE)/Section 22 - Building an AutoEncoder/aa.py
@@ -11,3 +11,10 @@ import os
 movies = pd.read_csv('ml-1m/movies.dat', sep='::', header=None, engine='python', encoding='latin-1')
 users = pd.read_csv('ml-1m/users.dat', sep='::', header=None, engine='python', encoding='latin-1')
 ratings = pd.read_csv('ml-1m/ratings.dat', sep='::', header=None, engine='python', encoding='latin-1')
+
+# Preparing the training set and the test set
+training_set = pd.read_csv('ml-100k/u1.base', delimiter='\t')
+training_set = np.array(training_set, dtype='int')
+
+test_set = pd.read_csv('ml-100k/u1.test', delimiter='\t')
+test_set = np.array(test_set, dtype='int')
