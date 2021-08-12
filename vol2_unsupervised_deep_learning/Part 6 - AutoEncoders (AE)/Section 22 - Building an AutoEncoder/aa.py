b8bf51eb38e04bc73f4b6b558910fd0928a59b33
Akila Mendis <akila@oneit.com.au>
2021-08-12 14:51:22 +0530

    Building an AutoEncoder - Step 1

---
+++ vol2_unsupervised_deep_learning/Part 6 - AutoEncoders (AE)/Section 22 - Building an AutoEncoder/aa.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.utils.data
+from torch.autograd import Variable
+import os
+
+# Importing the dataset
+movies = pd.read_csv('ml-1m/movies.dat', sep='::', header=None, engine='python', encoding='latin-1')
+users = pd.read_csv('ml-1m/users.dat', sep='::', header=None, engine='python', encoding='latin-1')
+ratings = pd.read_csv('ml-1m/ratings.dat', sep='::', header=None, engine='python', encoding='latin-1')
